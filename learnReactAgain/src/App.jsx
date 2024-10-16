e5d4473a34fafe618b0428144043991cf5aa7257
Duck-Dev687 <duckdev32@gmail.com>
2024-10-16 05:44:29 +0300

    made the list

--- learnReactAgain/src/App.jsx
+++ learnReactAgain/src/App.jsx
@@ -1,25 +1,50 @@
 import './App.css'
 import { React, useState } from 'react'
+import {FaTrashAlt} from 'react-icons/fa'
 
 function App() {
   const [items, setItems] = useState([
-    {id:1,item: "item1"},
-    {id:2,item: "item2"},
-    {id:3,item: "item3"},
+    {id:11,item: "item1", checked:true,},
+    {id:22,item: "item2", checked:false,},
+    {id:33,item: "item3", checked:true,},
   ])
   
+  const handleCheck = (id) =>{
+    const listItems = items.map(item => item.id === id? {...item, checked: !item.checked} : item)
+
+    setItems(listItems)
+
+    localStorage.setItem('shoppingList', JSON.stringify(listItems))
+  }
   
+  const handleDelete=(id)=>{
+    const listItems = items.filter((item)=>id !== item.id )
+
+    setItems(listItems)
+  }
+
   return (
     <>
+    {items.length? 
       <ul>
-        {items.map((item)=>(
-          <li className='item'>
-            <input type={item.item} key={item.id} />
-            <label htmlFor="item.item"></label>
-            <button>Delete</button>
-          </li>
-        ))}
-      </ul>
+      {items.map((item)=>(
+        <li className='item'>
+          <input
+          onChange={()=>handleCheck(item.id)}
+          type='checkbox' checked={item.checked} key={item.id} />
+        
+          <label 
+          style={(item.checked)? {textDecoration:'line-through'}:null}
+          onDoubleClick={()=>handleCheck(item.id)} >{item.item}</label>
+          <FaTrashAlt 
+          onClick={()=>handleDelete(item.id)}
+          role="button" tabIndex="0"></FaTrashAlt>
+        </li>
+      ))}
+    </ul>     
+     : <p>list is empty</p> 
+  }
+
     </>
   )
 }
