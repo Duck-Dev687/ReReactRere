1a6fef072ec744a4d2f2772820a903aad7bd74ef
Duck-Dev687 <duckdev32@gmail.com>
2024-10-20 19:28:54 +0300

    created apiRequest.jsx

---
+++ learnReactAgain/src/apiRequest.jsx
@@ -0,0 +1,11 @@
+const apiRequest = async (url = '',  optionsObj = null, errMsg =null)=>{
+    try{
+        const response = await fetch(url, optionsObj)
+        if(!response.ok) throw Error('Please reload the page');
+    } catch(errMsg){
+        errMsg = err.message;
+    } finally{
+        return errMsg;
+    }
+}
+export default apiRequest
