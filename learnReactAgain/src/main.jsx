52fef2905dd09f2ba8fa90b6b7e0b7078bece12a
Duck-Dev687 <duckdev32@gmail.com>
2024-10-20 16:53:30 +0300

    changed in main.jsx to the past defualt

--- learnReactAgain/src/main.jsx
+++ learnReactAgain/src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './Challenge1/App.jsx'
-import './Challenge1/style.css'
+// import App from './Challenge1/App.jsx'
+// import './Challenge1/style.css'
+
+import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
