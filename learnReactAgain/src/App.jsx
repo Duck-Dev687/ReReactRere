6c664c7fbadc8a09b0c8eedc35b5f348f8b8809c
Duck-Dev687 <duckdev32@gmail.com>
2024-10-20 14:31:04 +0300

    added Additem.jsx to App.jsx

--- learnReactAgain/src/App.jsx
+++ learnReactAgain/src/App.jsx
@@ -2,14 +2,22 @@ import './App.css'
 import { React, useState } from 'react'
 import {FaTrashAlt} from 'react-icons/fa'
 import Header from './Header'
+import AddItem from './AddItem'
 
 function App() {
   const [items, setItems] = useState([
-    {id:11,item: "item1", checked:true,},
+    {id:11 ,item: "item1", checked:true,},
     {id:22,item: "item2", checked:false,},
     {id:33,item: "item3", checked:true,},
   ])
   
+  const [newItem, setNewItem] = useState('')
+
+  const handleSubmmition = (e) =>{
+  alert("submitted!")
+
+  }
+
   const handleCheck = (id) =>{
     const listItems = items.map(item => item.id === id? {...item, checked: !item.checked} : item)
 
@@ -27,6 +35,8 @@ function App() {
   return (
     <>
     <Header title={"sdfsdf"}></Header>
+    <AddItem></AddItem>
+
     {items.length? 
       <ul>
       {items.map((item)=>(
