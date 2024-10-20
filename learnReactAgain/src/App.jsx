cf902c9cd1c670111f13e10e6e74d40e16eb84a1
Duck-Dev687 <duckdev32@gmail.com>
2024-10-20 17:29:34 +0300

    hook intro to useEffect

--- learnReactAgain/src/App.jsx
+++ learnReactAgain/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { React, useState } from 'react';
+import { React, useState, useEffect } from 'react';
 import { FaTrashAlt } from 'react-icons/fa';
 import Header from './Header';
 import AddItem from './AddItem';
@@ -10,36 +10,53 @@ function App() {
   const [newItem, setNewItem] = useState('');
   const [search, setSearch] = useState('');
 
+/*   
+
+// run every time the component render
+  useEffect(()=>{
+    console.log("render")
+  })
+
+  // run every time the component load bo[]
+  useEffect(()=>{
+    console.log("load")
+  }, [])
+  
+*/
+
+  console.log("before useEffect")
+  // run every time the dependecies change
+  useEffect(()=>{
+    console.log("inside useEffect")
+    }, [items])
+    
+  console.log("after useEffect")
+
+
   const setAndSaveItems = (newItems) => {
     setItems(newItems);
     localStorage.setItem('shoppingList', JSON.stringify(newItems));
   };
-
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
     setAndSaveItems(listItems);
   };
-
   const handleSubmmition = (e) => {
     e.preventDefault();
     if (!newItem) return;
     addItem(newItem);
     setNewItem('');
   };
-
   const handleCheck = (id) => {
     const listItems = items.map(item => item.id === id ? { ...item, checked: !item.checked } : item);
     setAndSaveItems(listItems);
   };
-
   const handleDelete = (id) => {
     const listItems = items.filter((item) => id !== item.id);
     setAndSaveItems(listItems);
   };
-
-  // Filter items based on the search input
   const filteredItems = items.filter((item) => 
     item.item.toLowerCase().includes(search.toLowerCase())
   );
