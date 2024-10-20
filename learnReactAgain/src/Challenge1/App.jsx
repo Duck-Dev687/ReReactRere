aadf14295a3ca9f1b830085551d1d0766183911c
Duck-Dev687 <duckdev32@gmail.com>
2024-10-20 16:52:01 +0300

    Done the Challenge1

--- learnReactAgain/src/Challenge1/App.jsx
+++ learnReactAgain/src/Challenge1/App.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './style.css'
 
 function App() {
-  return (
-    <div>App</div>
+  const [color, setColor] = useState('')
+//   let colorBox = document.getElementById('colorBox')
+
+//   const searchColor = (color) =>{
+//     style=`color: ${color}` ?  colorBox.style=`color: ${color}`  : colorBox.style=`color: white` 
+//   }
+
+
+// const filteredColor =(color)=>{
+//     color === 
+// }
+
+
+    return (
+    <>
+    <main>
+    <div 
+    style={{background: color}}
+    id='colorBox'
+    >
+        {color? <p>{color}</p>: <p>Empty Value</p>}
+    </div>
+
+    <input 
+    type="text"
+    placeholder='Add Color name'
+    value={color}
+    onChange={(e)=>setColor(e.target.value)}
+    />
+    </main>
+    </>
   )
 }
 
--- learnReactAgain/src/main.jsx
+++ learnReactAgain/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './Challenge1/App.jsx'
-import './index.css'
+import './Challenge1/style.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
