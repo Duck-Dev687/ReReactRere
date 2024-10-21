492df1eb00d908721edb2e93274884b1e266e08c
Duck-Dev687 <duckdev32@gmail.com>
2024-10-21 14:39:27 +0300

    Set boilerPlate for challnege2

--- learnReactAgain/src/main.jsx
+++ learnReactAgain/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import App from './Challenge1/App.jsx'
-// import './Challenge1/style.css'
+import App from './Challenge2/App.jsx'
+import './Challenge2/style.css'
 
-import App from './App.jsx'
-import './index.css'
+// import App from './App.jsx'
+// import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
