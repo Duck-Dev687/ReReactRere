517fbab0aa61db1621345b687c7a420a4a338c5e
Duck-Dev687 <duckdev32@gmail.com>
2024-10-21 18:28:36 +0300

    we'll stop hooks at here for today

--- learnReactAgain/src/App.jsx
+++ learnReactAgain/src/App.jsx
@@ -1,28 +1,30 @@
-import {React, useState, useEffect} from 'react'
-import './index.css'
-import Header from './Components/Header'
-import Nav from './Components/Nav'
-import Footer from './Components/Footer'
-import Home from './Components/Home'
-import NewPost from './Components/NewPost'
-import PostPage from './Components/PostPage'
-import About from './Components/About'
-import Missing from './Components/Missing'
-// import {Route, Switch, useHistory} from 'react-router-dom'
+import { React, useState, useEffect } from 'react';
+import './index.css';
+import Header from './Components/Header';
+import Nav from './Components/Nav';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import NewPost from './Components/NewPost';
+import PostPage from './Components/PostPage';
+import About from './Components/About';
+import Missing from './Components/Missing';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Header></Header>
-      <Nav></Nav>
-      <Home></Home>
-      <NewPost></NewPost>
-      <PostPage></PostPage>
-      <About></About>
-      <Missing></Missing>
-      <Footer></Footer>
+      <Header />
+      <Nav />
+      <Routes> {/* Change from Switch to Routes */}
+        <Route path='/' element={<Home />} /> {/* Use 'element' instead of children */}
+        <Route path='/post' element={<NewPost />} />
+        <Route path='/post/:id' element={<PostPage />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/*' element={<Missing />} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- learnReactAgain/src/main.jsx
+++ learnReactAgain/src/main.jsx
@@ -1,16 +1,15 @@
+import React from 'react';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom';
 
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route path='/' Component={App}/>
-      </Routes>
-    </Router>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   </StrictMode>,
 )
